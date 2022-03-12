99285355cb755fd75176739b86538917bcffe4ba
Chris N <chris.nelson.dev@gmail.com>
2022-03-11 17:39:27 -0700

    v1

--- wordblock.js
+++ wordblock.js
@@ -1,35 +1,88 @@
 class Wordblock{
-    constructor(output){
+    constructor(output, button){
         this.output = output;
+        this.button = button;
     }
 
     clear(){
         this.output.innerText = '';
-        countDiv.innerText = '0';
+    }
+
+    disableAll(){
+        for (let i = 0; i < button.length; i++) {
+            button[i].disabled = true;
+        }
+    }
+
+    enableAll(){
+        for (let i = 0; i < button.length; i++) {
+            button[i].disabled = false;
+        }
     }
 }
 
 
 const alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const startButton = document.getElementById('startButton');
+const gameDiv = document.getElementById('gameArea');
 const button = document.getElementsByClassName('btn');
 const output = document.getElementById('output');
 const submit = document.getElementById('submit');
 const clear = document.getElementById('clear');
 const countDiv = document.getElementById('count');
 let count = parseInt(countDiv.innerText);
+const scoreDiv = document.getElementById('score');
+let score = parseInt(scoreDiv.innerText);
+const timerDiv = document.getElementById('countDown');
+const gameOverScreen = document.getElementById('gameOver');
+let timeLeft = 60;
 
 let foundList = [];
 
-let game = new Wordblock(output);
+let game = new Wordblock(output, button);
+
+function gameOver(){
+    alert(
+        `You found ${foundList.length} words\nYou scored ${score} points\nStarting over..`
+    );
+    location.reload();
+}
+
+let timer = setInterval(function(){
+    if(timeLeft <= 0){
+        clearInterval(timer);
+        timerDiv.innerText = 'Game over!';
+        gameOver();
+    } else {
+        timerDiv.innerText = timeLeft;
+    }
+    timeLeft -= 1;
+}, 1000);
+
 
 function randInt(max){
     return Math.floor(Math.random() * max);
+}
+
+function getScore(word){
+    var pointAmount = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        pointAmount++;
+    }
+
+    return pointAmount;
 }
 
 
 for (let i in button){
     button[i].innerText = alph[randInt(25)];
 }
+
+startButton.addEventListener('click', ()=>{
+    gameDiv.style.display = 'block';
+    startButton.style.display = 'none';
+});
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', ()=>{
@@ -40,6 +93,7 @@ for (let i = 0; i < button.length; i++) {
         }
         
         output.innerText += pressed;
+        button[i].disabled = true;
     })
 }
 
@@ -54,12 +108,20 @@ submit.addEventListener('click', ()=>{
 
                     if(foundList.includes(data.words[i])){
                         console.log('You already found that word!');
+                        alert('You already found that word!');
                         game.clear();
+                        game.enableAll();
                     } else {
 
                         foundList.push(data.words[i]);
+
                         console.log('word matches: ' + data.words[i]);
+
                         game.clear();
+                        game.enableAll();
+                        score += getScore(data.words[i]);
+                        scoreDiv.innerText = score;
+                        console.log('score: ' + score);
 
                         count++;
                         countDiv.innerText = count;
@@ -71,11 +133,12 @@ submit.addEventListener('click', ()=>{
                 }
                 
             }
-
+            
         }
     ));
 })
 
 clear.addEventListener('click', ()=>{
     game.clear();
+    game.enableAll();
 })
